55fd0ace45b6bd21ddcda1b2cfc2efa6f7901c00
ecosmart-ai <64465558+ecosmart-ai@users.noreply.github.com>
2020-06-11 14:27:36 +0200

    Update BasicFeatures.py

--- NLP/Library/Statistical_Features/BasicFeatures.py
+++ NLP/Library/Statistical_Features/BasicFeatures.py
@@ -20,9 +20,12 @@ for i in range(0, len(Words)):
 # basic statistical features
 df['word_count'] = df["text"].apply(lambda x: len(str(x).split(" ")))
 df['char_count'] = df["text"].apply(lambda x: sum(len(word) for word in str(x).split(" ")))
-df['sentence_count'] = df["text"].apply(lambda x: len(str(x).split(".")))
 df['avg_word_length'] = df['char_count'] / df['word_count']
+
+df['sentence_count'] = df["text"].apply(lambda x: len(str(x).split(".")))
 df['avg_sentence_length'] = df['word_count'] / df['sentence_count']
+
+Words['STD_Ratings']=Words['Ratings'].apply(lambda x: (x-Words['Ratings'].mean())/Words['Ratings'].std())
 
 
 #cosine similarity based on tf-idf matrix 
